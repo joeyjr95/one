6c7770edf880684489147d38c15dc9f4b51036aa
joeyjr95 <joeyjr95@gmail.com>
2021-01-23 17:49:26 -0500

    added home module

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeDashboardComponent} from './home-dashboard/home-dashboard.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeDashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class HomeRoutingModule { }
+
+export const HomeRoutingComponents = [
+  HomeDashboardComponent
+]
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
+import { HomeRoutingModule, HomeRoutingComponents } from './home-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeRoutingComponents],
   imports: [
     CommonModule,
     HomeRoutingModule
