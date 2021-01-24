d44b0bc4e0498863f536e16124054c6e971c6ad1
joeyjr95 <joeyjr95@gmail.com>
2021-01-23 22:40:29 -0500

    more css and html

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { }
+  showMenu = false;
   ngOnInit(): void {
   }
 
+  toggleMenu = () => {
+    this.showMenu = !this.showMenu
+  }
+  goToHome = () => {
+    this.router.navigateByUrl('home')
+  }
 }
--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { }
+  showMenu = false;
   ngOnInit(): void {
   }
+
+  toggleMenu = () => {
+    this.showMenu = !this.showMenu
+  }
+  goToHome = () => {
+    this.router.navigateByUrl('home')
+  }
+
 
 }
